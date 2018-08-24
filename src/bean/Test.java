8a7a04f52c01637fa798b4e5d4c910030416409e
Keerthana-16 <36754605+Keerthana-16@users.noreply.github.com>
2018-08-24 11:34:51 +0530

    Update Test.java

--- src/bean/Test.java
+++ src/bean/Test.java
@@ -39,7 +39,7 @@ public class Test{
 		MysqlDataSource dataSource = new MysqlDataSource();
 		dataSource.setUser("root");
 		dataSource.setPassword("D3mo@dmin");
-		dataSource.setServerName("10.10.2.12");
+		dataSource.setServerName("10.10.2.186");
 		dataSource.setPortNumber(3306);
 		dataSource.setDatabaseName("demoDB");
 		 cx = dataSource.getConnection();
